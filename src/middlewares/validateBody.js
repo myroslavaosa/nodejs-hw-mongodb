b911e1bc36f04917dca33555e26ab4c103d29673
mira <mirasuper99@gmail.com>
2025-06-25 09:02:10 +0200

    first

---
+++ src/middlewares/validateBody.js
@@ -0,0 +1,14 @@
+// src/middlewares/validateBody.js
+
+export const validateBody = (schema) => async (req, res, next) => {
+    try {
+        await schema.validateAsync(req.body, { abortEarly: false });
+        next();
+    } catch (error) {
+        return res.status(400).json({
+            status: 400,
+            message: 'Validation error',
+            details: error.details.map(err => err.message),
+        });
+    }
+};
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,17 +1,21 @@
 import { Router } from 'express';
 import { getAllContacts, getContactById, createContactController, updateContactController, deleteContactController } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { createContactSchema } from '../validation/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
+
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getAllContacts));
 
-router.get('/:contactId', ctrlWrapper(getContactById));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
 
-router.post('/', ctrlWrapper(createContactController));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
 
-router.patch('/:contactId', ctrlWrapper(updateContactController));
+router.patch('/:contactId', isValidId, validateBody(createContactSchema), ctrlWrapper(updateContactController));
 
-router.delete('/:contactId', ctrlWrapper(deleteContactController));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
