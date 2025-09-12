4ff18cf194132c675a1eef1b453414fa9c77284f
mira <mirasuper99@gmail.com>
2025-09-12 12:27:40 +0200

    swagger docs

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,17 +1,22 @@
 import swaggerUI from 'swagger-ui-express';
-import YAML from 'yamljs';
+import SwaggerParser from '@apidevtools/swagger-parser';
 import { SWAGGER_PATH } from '../constants/index.js';
 
 export const swaggerDocs = () => {
-    try {
-        const swaggerDoc = YAML.load(SWAGGER_PATH);
-        return [swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-    } catch (err) {
-        return [
-            (req, res, next) => {
-                console.error("Can't load swagger docs:", err);
+    let swaggerDoc;
+
+    return [
+        async (req, res, next) => {
+            try {
+                if (!swaggerDoc) {
+                    swaggerDoc = await SwaggerParser.bundle(SWAGGER_PATH); // 🔥 löst alle $ref auf
+                }
+                return swaggerUI.setup(swaggerDoc)(req, res, next);
+            } catch (err) {
+                console.error("Swagger load error:", err);
                 res.status(500).json({ message: "Can't load swagger docs" });
             }
-        ];
-    }
+        },
+        swaggerUI.serve,
+    ];
 };
